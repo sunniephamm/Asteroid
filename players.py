f1b45e32bdfb25952e98cb4f6aa22ab89590f30d
sunniephamm <147637478+sunniephamm@users.noreply.github.com>
2023-11-20 10:58:56 -0500

    Update to players.py

--- players.py
+++ players.py
@@ -1,5 +1,12 @@
 import pygame
 
+screen_width = 800
+screen_height = 600
+MIN_SPEED = .5 #pixels per frame
+MAX_SPEED = 3
+PLAYER_SPEED = 3.0
+
+NUM_LIVES = 3
 class Player(pygame.sprite.Sprite):
     def __init__(self, x,y):
         super().__init__()
@@ -14,3 +21,40 @@ class Player(pygame.sprite.Sprite):
         self.rect.center = (x, y)
         self.x_speed = 0
         self.y_speed = 0
+
+    def move_up(self):
+        self.y_speed = -1*PLAYER_SPEED
+
+    def move_down(self):
+        self.y_speed = 1*PLAYER_SPEED
+
+    def move_left(self):
+        self.x_speed = -1*PLAYER_SPEED
+        self.forward_image = self.backward_image
+
+    def move_right(self):
+        self.x_speed = PLAYER_SPEED
+        self.forward_image = pygame.image.load("../assets/sprites/orange_fish.png").convert()
+        self.forward_image.set_colorkey((0, 0, 0))
+    def stop(self):
+        self.y_speed = 0
+        self.x_speed = 0
+
+    def update(self):
+        #TODO to check if player fish went off the screen
+        #update the x position
+        self.x += self.x_speed
+        self.y += self.y_speed
+        self.rect.x = self.x
+        self.rect.y = self.y
+        if self.rect.x > screen_width-50:
+            self.x_speed = 0
+        if self.rect.x < 0:
+            self.x_speed = 0
+        if self.rect.y > screen_height-100:
+            self.y_speed = 0
+        if self.rect.y < 0:
+            self.y_speed = 0
+
+    def draw(self, screen):
+        screen.blit(self.forward_image, self.rect)
